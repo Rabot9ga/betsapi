409af024e1fbb4f0a3a2ae62201939b72a0291b5
Rabot9ga <freedomboxnikita@inbox.ru>
2022-08-27 09:56:30 +0300

    'pep_edit'

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
 from bs4 import BeautifulSoup
 import csv
 
+a = {}
+
+
+def adding_a(time, c):
+    if time in a:
+        a[time] += c
+    else:
+        a[time] = c
+
+
 for k in range(1, 15):
-    a={}
-    with open(str(k)+'.html', 'r', encoding='utf-8') as f:
+    with open(str(k) + '.html', 'r', encoding='utf-8') as f:
         soup = BeautifulSoup(f.read(), 'html.parser')
         for i in range(0, 24):
             for j in range(0, 60):
-                if i<10 and j<10:
-                    c=soup.get_text().count('0'+str(i)+':'+'0'+str(j))
+                if i < 10 and j < 10:
+                    c = soup.get_text().count('0' + str(i) + ':' + '0' + str(j))
+                    time = '0' + str(i) + ':' + '0' + str(j)
                     if c:
-                        a['0'+str(i)+':'+'0'+str(j)]=c
-                elif i<10 and j>9:
+                        adding_a(time, c)
+                elif i < 10 and j > 9:
                     c = soup.get_text().count('0' + str(i) + ':' + str(j))
+                    time = '0' + str(i) + ':' + str(j)
                     if c:
-                        a['0' + str(i) + ':' + str(j)] = c
-                elif i>9 and j<10:
-                    c = soup.get_text().count(str(i) + ':' + '0'+str(j))
+                        adding_a(time, c)
+                elif i > 9 and j < 10:
+                    c = soup.get_text().count(str(i) + ':' + '0' + str(j))
+                    time = str(i) + ':' + '0' + str(j)
                     if c:
-                        a[str(i) + ':' + '0'+str(j)] = c
+                        adding_a(time, c)
                 else:
                     c = soup.get_text().count(str(i) + ':' + str(j))
+                    time = str(i) + ':' + str(j)
                     if c:
-                        a[str(i) + ':' + str(j)] = c
+                        adding_a(time, c)
 
-    with open(str(k)+'.txt', 'w') as file:
-        for i in a:
-            file.write(str(i)+','+str(a[i])+'\n')
-
-
-
-
-
-
-
-
+with open('final.txt', 'w') as file:
+    for i in a:
+        file.write(str(i) + ',' + str(a[i]) + '\n')
