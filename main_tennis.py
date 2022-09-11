0e59caa8bd817afa5adeae3d47c6f42a58e818b9
Rabot9ga <freedomboxnikita@inbox.ru>
2022-09-11 14:04:02 +0300

    're_use'

--- main_tennis.py
+++ main_tennis.py
@@ -1,46 +1,24 @@
 from bs4 import BeautifulSoup
-import csv
+import re
 
 def main(pages_ten):
 
     a = {}
 
 
-    def adding_a(time, c):
-        if time in a:
-            a[time] += c
-        else:
-            a[time] = c
+
 
 
     for k in range(1, pages_ten):
         with open('html\\' + str(k) + 'tennis.html', 'r', encoding='utf-8') as f:
             print(f)
             soup = BeautifulSoup(f.read(), 'html.parser')
-            for i in range(0, 24):
-                for j in range(0, 60):
-                    if i < 10 and j < 10:
-                        c = soup.get_text().count('0' + str(i) + ':' + '0' + str(j))
-                        time = '0' + str(i) + ':' + '0' + str(j)
-                        if c:
-                            adding_a(time, c)
-                    elif i < 10 and j > 9:
-                        c = soup.get_text().count('0' + str(i) + ':' + str(j))
-                        time = '0' + str(i) + ':' + str(j)
-                        if c:
-                            adding_a(time, c)
-                    elif i > 9 and j < 10:
-                        c = soup.get_text().count(str(i) + ':' + '0' + str(j))
-                        time = str(i) + ':' + '0' + str(j)
-                        if c:
-                            adding_a(time, c)
-                    else:
-                        c = soup.get_text().count(str(i) + ':' + str(j))
-                        time = str(i) + ':' + str(j)
-                        if c:
-                            adding_a(time, c)
-
-    print(a)
+            lst = re.findall(r"\d\d:\d\d", soup.get_text())
+            for i in lst:
+                if i in a:
+                    a[i]+=1
+                else:
+                    a[i]=1
 
     with open('final_tennis.txt', 'w') as file:
         for i in a:
